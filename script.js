f834e2ef179156d7245aedf4c84bd601021adb08
Jessica D <deck.jessica@gmail.com>
2020-11-08 13:26:50 -0500

    started building out questions array, building out html to house questions, and start button

---
+++ script.js
@@ -0,0 +1,12 @@
+var questions = [{
+    question: "Which element of HTML do we put the Javascript inside of?",
+    choices: ["<scripting>", "<script>","<javascript>","<js>"],
+    correctAnswer: 1 }, 
+    { question: "Where is the correct place to insert Javascript in HTML?",
+        choices: ["the <head> section", "the <body> section", "Both the head and body sections are correct"],
+        correctAnswer: 0 },
+   {  question: "How do you write 'Hello World' in an alert box?",
+        choices: ["alertBox('Hello World');", "msgBox('Hello World');","msg('Hello World');","alert('Hello World');"],
+         correctAnswer: 3 },
+
+]
